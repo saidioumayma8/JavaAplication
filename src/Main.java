b53b6530515a91ea63e46be6d756bee9d17bb624
Oumayma Saidi <your_email@example.com>
2024-12-10 17:19:36 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] arguments) {
+        int operator, n1, n2;
+        System.out.println("1- Add(+) \n2- Soustraction(-) \n3-Multiplication(*) \n4-Division(/) \n5-Puissance \n6-Racine carrée \n7-Factorielle");
+        System.out.println("Chosse Operator: ");
+        Scanner sc = new Scanner(System.in);
+        operator = sc.nextInt();
+        System.out.println("Entre first number : ");
+        n1 = sc.nextInt();
+        System.out.println("Entre second number : ");
+        n2 = sc.nextInt();
+
+        int result = 0;
+        switch(operator){
+            case 1:
+                result = n1 + n2;
+                break;
+            case 2:
+                result = n1 - n2;
+                break;
+            case 3:
+                result = n1 + n2;
+                break;
+            case 4:
+                result = n1 * n2;
+                break;
+            case 5:
+                result = n1 / n2;
+                break;
+            case 6:
+                result = n1 + n2;
+                break;
+            case 7:
+                result = n1 + n2;
+                break;
+        }
+    }
+}
